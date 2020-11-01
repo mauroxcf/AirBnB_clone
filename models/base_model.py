da13d39a5d65d899ecf7febe8670db0303bccaaa
I7RANK <franfgh2000@hotmail.com>
2020-10-31 19:12:17 -0500

    uploading tasks 3..5

---
+++ models/base_model.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""this module contains the Base CLass for the project Airbnb_clone
+"""
+
+
+import uuid
+import datetime
+import models
+
+
+dati = datetime.datetime
+
+
+class BaseModel():
+    """
+    Base class for all class in airbnb_clone project
+    """
+    def __init__(self, *args, **kwargs):
+        """Constructor
+        """
+        if kwargs:
+            new_dict = kwargs["data"]
+            for k, v in new_dict.items():
+                if k != "__class__":
+                    if k == "created_at" or k == "updated_at":
+                        val = dati.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                        self.__dict__[k] = val
+                    else:
+                        self.__dict__[k] = v
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = dati.now()
+            self.updated_at = dati.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """Returns a string with this format [<class name>] (<self.id>) <self.__dict__>
+
+        Returns:
+            str: format - [<class name>] (<self.id>) <self.__dict__>
+        """
+        cl_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(cl_name, self.id, self.__dict__)
+
+    def save(self):
+        """updates the update_at atrribute
+        """
+        self.updated_at = dati.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """Returns a dict with all values of __dict__
+            adds a new key('__class__') with the self class name
+            converts created_at and updated_at to str in ISO format
+
+        Returns:
+            dict: new_dict
+        """
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""This module cintains the FileStorage class
+"""
+
+
+import json
+import datetime
+from models.base_model import BaseModel
+
+
+class FileStorage():
+    def __init__(self):
+        """ constructor """
+        self.__file_path = "file.json"
+        self.__objects = {}
+
+    def all(self):
+        """returns the dictionary __objects
+
+        Returns:
+            dict: dictionary of all objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """adds a new objects in __objects
+
+        Args:
+            obj (obj): one objects
+        """
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj
+
+    def save(self):
+        """saves the serialization of __objects in one file.json
+        """
+        """ try:
+            with open(__file_path) as fl:
+                json_str = fl.read()
+                if json_str != '':
+                    temp_obj = json.loads(json_str)
+        except FileNotFoundError:
+            with open(__file_path, "w") as fl:
+                pass """
+        with open(self.__file_path, "w") as fl:
+            if not self.__objects:
+                fl.write("{}")
+            else:
+                temp_dict = {}
+                for key1 in self.__objects.keys():
+                    temp_dict[key1] = self.__objects[key1].to_dict()
+                fl.write(json.dumps(temp_dict))
+
+    def reload(self):
+        """ deserializes the JSON file to """
+        """if __file_path: """
+        try:
+            with open(self.__file_path, "r") as file1:
+                """ str_file = str(file1.read()) """
+                temp = json.load(file1)
+
+                for key in temp.keys():
+                    new_instance = BaseModel(data=temp[key])
+                    self.__objects[key] = new_instance
+        except FileNotFoundError:
+            pass
